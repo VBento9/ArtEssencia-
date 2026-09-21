d112f097a74bc3e1bb77d3d6e302a678e37b4aeb
VBento9 <163012722+VBento9@users.noreply.github.com>
2026-09-22 00:40:36 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='artessencia-v1-7-62';
+const CACHE='artessencia-v1-7-63';
 const CORE=['./','./index.html','./manifest.webmanifest','./brand-artessencia.png','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).catch(()=>{}));self.skipWaiting()});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
